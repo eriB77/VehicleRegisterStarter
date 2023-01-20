cb017f68ee9e949928bf457a65c7de8ba9570e68
Erika Birtha <ebirtha@hotmail.com>
2023-01-20 17:51:00 +0100

    use controller

--- src/main/java/hu/erikabirtha/Main.java
+++ src/main/java/hu/erikabirtha/Main.java
@@ -1,5 +1,16 @@
-package hu.erikabirtha;public class Main {
+package hu.erikabirtha;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //interactor
+        VehiclePersistence vp = new FileStorage();
+        Display presenter = new UIPresenter();
+        Contoller contoller = new Contoller();
+
+        VehicleRegisterAPI vehicleRegisterInteractor = new VehicleRegisterInteractor(vp, presenter);
+
+        contoller.setVehicleRegister(vehicleRegisterInteractor);
+        contoller.mainLoop();
     }
 }
+//console run
